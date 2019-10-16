5b2d933c7103b00ac65292d65e2213b36edeee16
lxcyha <253314157@qq.com>
2019-10-17 04:51:45 +0800

    speed up

--- ThreadSecurity.java
+++ ThreadSecurity.java
@@ -1,26 +1,36 @@
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
 
+public class ThreadSecurity implements Runnable {
 
-class MyThread implements Runnable {
-    public static int count = 0;
+    private static int count = 0;
+    private static int threadCount = 100;
+    private static CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < threadCount; i++) {
+            Thread thread = new Thread(new ThreadSecurity());
+            thread.start();
+        }
+
+        try {
+            countDownLatch.await();
+            System.out.println(count);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void run() {
         counting();
     }
 
     public void counting() {
-        count++;
-        System.out.println(count);
-    }
-}
-
-public class ThreadSecurity {
-
-    public static void main(String[] args) {
         for (int i = 0; i < 100000; i++) {
-            Thread thread = new Thread(new MyThread());
-            thread.start();
+            count++;
         }
+        countDownLatch.countDown();
     }
 
 }
