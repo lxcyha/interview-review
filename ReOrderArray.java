cdd8be4c0cdebb67ed1425f40d45b179a8021208
lxcyha <253314157@qq.com>
2019-10-21 23:12:37 +0800

    ReOrderArray

---
+++ ReOrderArray.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ReOrderArray {
+    private static void reOrder(int[] array) {
+        List<Integer> oddList = new ArrayList<>();
+        List<Integer> evenList = new ArrayList<>();
+
+        for (int value : array) {
+            if (value % 2 == 0) {
+                evenList.add(value);
+                continue;
+            }
+            oddList.add(value);
+        }
+
+        oddList.addAll(evenList);
+
+        for (int i = 0; i < oddList.size(); i++) {
+            array[i] = oddList.get(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8,0};
+        reOrder(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
